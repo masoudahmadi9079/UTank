d34ef6986b85331e937dcbb7581f5043bf4f39be
Mahdi Tavakoli <goo99p@gmail.com>
2020-02-02 20:11:49 +0330

    fixed directions and angularVelocity

--- src/Data/Tank.java
+++ src/Data/Tank.java
@@ -7,7 +7,7 @@ public class Tank extends MovingThing {
     private final static int GUN_LENGTH = 30;
 
     Tank(int x, int y, double direction) {
-        super(x, y, direction, 2f, 0.3f);
+        super(x, y, direction, 2f, 0.06f);
     }
 
     public void draw(Graphics graphics) {
